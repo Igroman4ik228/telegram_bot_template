3c041fcb9482ef755056334645271d51aa955779
igroman4ik-pc <ni4000009@gmail.com>
2025-10-08 19:16:59 +0300

    add di container (rodi)

--- src/__main__.py
+++ src/__main__.py
@@ -1,21 +1,39 @@
 from app.services.crud.user import UserCrudService
+from container import AppContainer
 from domain.entities.user import UserEntity
 from infrastructure.database.db import AlchemyDatabase
-from infrastructure.database.uow import AlchemyUoW
+
+
+class App:
+    def __init__(self, container: AppContainer):
+        self.db = container.resolve(AlchemyDatabase)
+        self.user_service = container.resolve(UserCrudService)
+
+    def start(self) -> None:
+        # Delete this after testing
+        self.db.create_tables()
+
+        new_user = UserEntity(username="johndoe", fullname="John Doe")
+
+        self.user_service.register_user(new_user)
+
+        retrieved_user = self.user_service.get(1)
+        if retrieved_user:
+            print(repr(retrieved_user))  # Output: Hi, John Doe
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
 
 
 def main() -> None:
-    db = AlchemyDatabase()
-    db.create_tables()
+    app_container = AppContainer()
+    app_container.register_di()
 
-    user_service = UserCrudService(AlchemyUoW(db.session_maker))
-
-    new_user = UserEntity(username="johndoe", fullname="John Doe")
-    user_service.register_user(new_user)
-
-    retrieved_user = user_service.get(1)
-    if retrieved_user:
-        print(repr(retrieved_user))  # Output: Hi, John Doe
+    with App(app_container) as app:
+        app.start()
 
 
 if __name__ == "__main__":
---
+++ src/container.py
@@ -0,0 +1,24 @@
+from rodi import Container
+
+from app.interfaces.uow import IUoW
+from app.services.crud.user import UserCrudService
+from infrastructure.database.db import AlchemyDatabase
+from infrastructure.database.uow import AlchemyUoW
+
+
+class AppContainer(Container):
+    def __init__(self):
+        super().__init__()
+
+    def register_di(self) -> None:
+        self.add_singleton_by_factory(
+            lambda: AlchemyDatabase(),
+            AlchemyDatabase,
+        )
+        self.add_scoped_by_factory(
+            lambda c: AlchemyUoW(c.get(AlchemyDatabase).session_maker),
+            IUoW,
+        )
+        self.add_scoped(
+            UserCrudService,
+        )
